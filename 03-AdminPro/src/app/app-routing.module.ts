216a6bf1d4a4aa23ba7cf3bdeefb9bcda605c196
Luciano Festa <lucianoefesta@hotmail.com>
2023-08-17 14:47:48 -0300

    Fina módulo 4: Módulos y routiing

--- 03-AdminPro/src/app/app-routing.module.ts
+++ 03-AdminPro/src/app/app-routing.module.ts
@@ -1,36 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
 
 const routes:Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      //Rutas hijas que mantienen el estilo y son las que necesito cuando el usuario esta autenticado.
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    ]
-  },
 
-  //Estas rutas tendrán sus propias vistas diferentes a las de arriba.
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: NopagesfoundComponent },
+
 ]
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ 
+    RouterModule.forRoot(routes), 
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
